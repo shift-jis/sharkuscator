2a0d019f1b4069bb4b285668e610be36d954195f
shitposts <66158890+RabiesDev@users.noreply.github.com>
2025-05-24 02:37:23 +0900

    Partial support for Minecraft mod native obfuscation

--- sharkuscator.obfuscator/src/main/kotlin/dev/sharkuscator/obfuscator/extensions/MethodNodeExtension.kt
+++ sharkuscator.obfuscator/src/main/kotlin/dev/sharkuscator/obfuscator/extensions/MethodNodeExtension.kt
@@ -17,7 +17,23 @@ fun MethodNode.isDeclaredSynthetic(): Boolean = (node.access and Opcodes.ACC_SYN
 
 fun MethodNode.isDeclaredBridge(): Boolean = (node.access and Opcodes.ACC_BRIDGE) != 0
 
-fun MethodNode.isMixinAccessor(): Boolean = node.visibleAnnotations?.any { it.desc == "Lorg/spongepowered/asm/mixin/gen/Accessor;" } ?: false
+fun MethodNode.isMixinAccessor(): Boolean = hasVisibleAnnotation("Lorg/spongepowered/asm/mixin/gen/Accessor;")
+
+fun MethodNode.hasVisibleAnnotationContaining(descriptor: String): Boolean {
+    return node.visibleAnnotations?.any { it.desc.contains(descriptor) } ?: false
+}
+
+fun MethodNode.hasVisibleAnnotation(descriptor: String): Boolean {
+    return node.visibleAnnotations?.any { it.desc == descriptor } ?: false
+}
+
+fun MethodNode.hasInVisibleAnnotationContaining(descriptor: String): Boolean {
+    return node.invisibleAnnotations?.any { it.desc.contains(descriptor) } ?: false
+}
+
+fun MethodNode.hasInVisibleAnnotation(descriptor: String): Boolean {
+    return node.invisibleAnnotations?.any { it.desc == descriptor } ?: false
+}
 
 fun MethodNode.shouldSkipTransform(): Boolean {
     return isStaticInitializer() || isConstructor() || hasMainSignature()
