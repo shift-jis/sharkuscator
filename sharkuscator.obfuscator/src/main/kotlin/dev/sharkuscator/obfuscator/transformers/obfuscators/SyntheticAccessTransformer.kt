9b8e59031b8ec77bcf0510d1bcc12c43891de5f1
Toshimichi0915 <main@toshimichi.net>
2025-06-17 17:51:58 +0900

    Add SyntheticAccessTransformerConfiguration.kt

--- sharkuscator.obfuscator/src/main/kotlin/dev/sharkuscator/obfuscator/transformers/obfuscators/SyntheticAccessTransformer.kt
+++ sharkuscator.obfuscator/src/main/kotlin/dev/sharkuscator/obfuscator/transformers/obfuscators/SyntheticAccessTransformer.kt
@@ -1,13 +1,26 @@
 package dev.sharkuscator.obfuscator.transformers.obfuscators
 
+import dev.sharkuscator.obfuscator.configuration.GsonConfiguration
+import dev.sharkuscator.obfuscator.configuration.transformers.SyntheticAccessTransformerConfiguration
 import dev.sharkuscator.obfuscator.configuration.transformers.TransformerConfiguration
 import dev.sharkuscator.obfuscator.events.TransformerEvents
 import dev.sharkuscator.obfuscator.extensions.*
 import dev.sharkuscator.obfuscator.transformers.BaseTransformer
+import dev.sharkuscator.obfuscator.transformers.obfuscators.renamers.FieldRenameTransformer
 import meteordevelopment.orbit.EventHandler
 import org.objectweb.asm.Opcodes
+import org.objectweb.asm.Type
 
-object SyntheticAccessTransformer : BaseTransformer<TransformerConfiguration>("SyntheticAccess", TransformerConfiguration::class.java) {
+object SyntheticAccessTransformer : BaseTransformer<SyntheticAccessTransformerConfiguration>("SyntheticAccess", SyntheticAccessTransformerConfiguration::class.java) {
+
+    lateinit var excludeAnnotations: List<Regex>
+
+    override fun initialization(configuration: GsonConfiguration): SyntheticAccessTransformerConfiguration {
+        super.initialization(configuration)
+        excludeAnnotations = this.configuration.excludeAnnotations.map { Regex(it) }
+        return this.configuration
+    }
+
     @EventHandler
     @Suppress("unused")
     private fun onClassTransform(event: TransformerEvents.ClassTransformEvent) {
@@ -29,6 +42,10 @@ object SyntheticAccessTransformer : BaseTransformer<TransformerConfiguration>("S
     @Suppress("unused")
     private fun onFieldTransform(event: TransformerEvents.FieldTransformEvent) {
         if (exclusions.excluded(event.anytypeNode) || event.anytypeNode.isDeclaredVolatile() || event.anytypeNode.isDeclaredSynthetic() || event.anytypeNode.isDeclaredBridge()) {
+            return
+        }
+
+        if (event.anytypeNode.node.visibleAnnotations?.any { annot -> FieldRenameTransformer.excludeAnnotations.any { it.matches(Type.getType(annot.desc).internalName) } } ?: false) {
             return
         }
 
